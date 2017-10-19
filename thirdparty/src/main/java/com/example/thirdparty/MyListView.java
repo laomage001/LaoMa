fd7512c7bae88494e18fda4ac2940eda352f9ded
dell <862685960@qq.com>
2017-10-19 19:11:56 +0800

    my project

---
+++ thirdparty/src/main/java/com/example/thirdparty/MyListView.java
@@ -0,0 +1,47 @@
+package com.example.thirdparty;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * @author Dash
+ * @date 2017/10/19
+ * @description:
+ *
+ * 参考博客地址:http://blog.csdn.net/zhaokaiqiang1992/article/details/38585547
+ */
+public class MyListView extends ListView {
+    public MyListView(Context context) {
+        super(context);
+    }
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 重新计算高度
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+   /* @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }*/
+}
